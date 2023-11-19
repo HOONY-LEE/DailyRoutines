6b66822064d84d1958659e802d7d1bb61579cb17
Sunghoon Lee <leesh940312@gmail.com>
2023-11-19 16:10:06 +0900

    [feat] project settings

---
+++ daily-routine/src/components/header/header.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function header() {
+  return (
+    <div>여기가 헤더입니다.</div>
+  )
+}
---
+++ daily-routine/src/components/main/mainRoutineListComponent.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function routineListComponent() {
+  return (
+    <div>routineListComponent</div>
+  )
+}
